70e11451c42b758b59452f4b1386834a75bed92a
Josue Benjamin <josuebcenturion@gmail.com>
2022-11-18 00:15:32 -0300

    fixing app, and adding js

--- src/components/HomePage.jsx
+++ src/components/HomePage.jsx
@@ -1,63 +1,75 @@
-import {
-  Card,
-  Page,
-  Layout,
-  TextContainer,
-  Image,
-  Stack,
-  Link,
-  Heading,
-} from "@shopify/polaris";
-
-import trophyImgUrl from "../assets/home-trophy.png";
-import React from "react";
-import { ProductsCard } from "./ProductsCard";
-import { NavBar } from "./NavBar";
-import { ResourcePicker } from "@shopify/app-bridge-react";
-
+import React from 'react';
+import gql from 'graphql-tag';
+import { Mutation } from '@apollo/client/react/components';
+import ResourceListWithProducts from '../graphql/ResourceList';
+import store from 'store-js';
+import { Heading, Page, TextStyle, Layout, EmptyState } from "@shopify/polaris";
+import { ResourcePicker, TitleBar } from '@shopify/app-bridge-react';
+const img = 'https://cdn.shopify.com/s/files/1/0757/9955/files/empty-state.svg';
 
 
 class HomePage extends React.Component {
-  state = {
-    open:false, 
+  state = { open: false };
+  render() {
+    // A constant that defines your app's empty state
+    const emptyState = !store.get('ids');
+    return (
+      <Page>
+        <TitleBar
+          primaryAction={{
+            content: 'Select products',
+            onAction: () => this.setState({ open: true }),
+          }}
+        />
+        <ResourcePicker
+          resourceType="Product"
+          showVariants={false}
+          open={this.state.open}
+          onSelection={(resources) => this.handleSelection(resources)}
+          onCancel={() => this.setState({ open: false })}
+        />
+        {emptyState ? ( // Controls the layout of your app's empty state
+          <Layout>
+            <EmptyState
+              heading="Discount your products temporarily"
+              action={{
+                content: 'Select products',
+                onAction: () => this.setState({ open: true }),
+              }}
+              image={img}
+            >
+              <p>Select products to change their price temporarily.</p>
+            </EmptyState>
+          </Layout>
+        ) : (
+          // Uses the new resource list that retrieves products by IDs
+          <ResourceListWithProducts />
+        )}
+      </Page>
+    );
   }
-  render() 
-  {
-  return (
-    <Page fullWidth
-    title="Product Selector"
-    primaryAction={{
-      content: "Select Products",
-      onAction: () => this.setState({open:true})
-    }}
-    >
-      <Layout>
-      <Layout.Section>
-          <NavBar />
-        </Layout.Section>
-        <Layout.Section secondary>
-          <ProductsCard />
-        </Layout.Section>
-        <Layout.Section>
-          <ResourcePicker 
-            resourceType="Product"
-            open={this.state.open}
-            onCancel={() => this.setState({open:false})}
-            onSelection={(resorces) => this.handleSelection(resorces) }
-          />
-        </Layout.Section>
-
-      </Layout>
-    </Page>
-  );
-}
   handleSelection = (resources) => {
-    const idList = resources.selection.map((productId) => productId.id)
-  this.setState({open:false}) 
-  console.log(idList);
+    const idsFromResources = resources.selection.map((product) => product.id);
+    this.setState({ open: false });
+    store.set('ids', idsFromResources);
+  };
 }
+export default HomePage;
 
 
-}
 
-export default HomePage 
+/* NEXT STEPS 
+
+In your app, you can now update the prices of products.
+
+![GIF showing how to populate data in a development store](https://shopify.dev/assets/apps/randomize-prices-a8c49c220e447a3b5ac233f582eddd2a9bc81050c32c601b5de6ae99001e8ae8.gif)
+
+
+
+
+## Next steps[](https://shopify.dev/apps/getting-started/add-functionality#next-steps)
+
+-   Use  [webhooks](https://shopify.dev/apps/webhooks)  to stay in sync with Shopify or execute code after a specific event occurs in a shop.
+-   Identify your  [app business model](https://shopify.dev/apps/billing/models)  and learn how to use the  [Billing API](https://shopify.dev/apps/billing)  to bill customers with recurring monthly charges or one-time purchases.
+-   Learn how to use  [app extensions](https://shopify.dev/apps/app-extensions)  to add features to Shopify admin or POS.
+-   Explore the  [GraphQL Admin API](https://shopify.dev/api/admin/graphql/reference)  and  [REST Admin API](https://shopify.dev/api/admin/rest/reference)  references.*/
